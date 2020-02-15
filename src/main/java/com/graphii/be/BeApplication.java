f1e3e82c83043f2b096af79920384d8115de3e7b
YutaSeki36 <sekiyuta0306@gmail.com>
2020-02-15 15:21:34 +0900

    cors対応

--- src/main/java/com/graphii/be/BeApplication.java
+++ src/main/java/com/graphii/be/BeApplication.java
@@ -2,7 +2,12 @@ package com.graphii.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @SpringBootApplication
 public class BeApplication {
 
@@ -10,4 +15,15 @@ public class BeApplication {
 		SpringApplication.run(BeApplication.class, args);
 	}
 
+	private String graphqlurl="http://localhost:8080/graphiql";
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/graphiql").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 }
